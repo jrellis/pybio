83a56dc31294ff210373274fd4a2042137594a84
jrellis <jrobertellis@gmail.com>
2015-06-24 22:48:06 -0400

    Add basic skeleton

---
+++ pybio/sequence.py
@@ -0,0 +1,8 @@
+""" A Sequence class for biosequences """
+from __future__ import print_function
+
+__all__ = ['Sequence']
+
+class Sequence(object):
+  def __init__(self):
+    pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='pybio',
+      version='0.1',
+      description='A bioinformatics sequence library',
+      url='https://github.com/jrellis/pybio',
+      author='Rob Ellis',
+      author_email='jrellis@fas.harvard.edu',
+      packages=['pybio'])
+
