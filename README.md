c465d0c07953a316c1396a01c97dabfeee71cedf
jrellis <jrobertellis@gmail.com>
2015-10-15 22:46:22 -0400

    cythonize and cython_develop commands for setup.py

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # pybio
 
 A new bioinformatics library for python.
+
+To install from pip:
+pip install pybio
+
+To install from source:
+python setup.py install
+
+To compile the cython:
+python setup.py cythonize
+
+To compile the cython and install develop:
+python setup.py cython_develop
--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
-from setuptools import setup, Extension, find_packages
-from Cython.Build import cythonize
+from setuptools import setup, Extension, find_packages, Command
+from setuptools.command.develop import develop
 import os
+
+cython_globs = ['pybio/alignment/*.pyx']
+
+class cythonize(Command):
+    description = 'Builds cython files.'
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        from Cython.Build import cythonize
+        cythonize(cython_globs)
+
+class cython_develop(develop):
+    description = 'Builds cython files and runs develop.'
+    def run(self):
+        from Cython.Build import cythonize
+        cythonize(cython_globs)
+        develop.run(self)
 
 def setup_package():
 
     metadata = dict(
             name='pybio',
-            version='0.116',
+            version='0.121',
             description='A bioinformatics sequence library',
             url='https://github.com/jrellis/pybio',
             author='Rob Ellis',
             author_email='jrellis@fas.harvard.edu',
-            packages=['pybio'],
-            ext_modules = cythonize([Extension("*", ["pybio/alignment/*.pyx","pybio/alignment/lib/ssw.c"])]),
-            install_requires=['cython', 'numpy'],
+            packages=['pybio', 'pybio.alignment'],
+            ext_modules = [
+                Extension('pybio.alignment.smith_waterman',sources=['pybio/alignment/smith_waterman.c', 'pybio/alignment/lib/ssw.c'],depends=['pybio/alignment/lib/ssw.h'])
+                ],
+            cmdclass = {'cythonize':cythonize, 'cython_develop':cython_develop},
+            install_requires=['numpy', 'setuptools'],
             )
 
     setup(**metadata)
